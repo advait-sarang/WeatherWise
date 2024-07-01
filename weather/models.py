c9fe26d3120711a10d6877bee85a8c4427e5a4b3
Advait <advaits6895@gmail.com>
2024-07-02 02:47:33 +0530

    Commit 02-07-2024

--- weather/models.py
+++ weather/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-
 class City(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=100)
 
-    def __str__(self):  # show the actual city name on the dashboard
+    def __str__(self):
         return self.name
-
-    class Meta:  # show the plural of city as cities instead of cities
-        verbose_name_plural = 'cities'
--- weather/views.py
+++ weather/views.py
@@ -1,35 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from .models import City
 from .forms import CityForm
 
-
 def index(request):
-    cities = City.objects.all()  # return all the cities in the database
+    # Fetch all cities from the database
+    cities = City.objects.all()
 
-    url = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=imperial&appid=ac54df7369427aca98b7f2da1d1888b8"
+    # Define the base URL for OpenWeatherMap API
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ac54df7369427aca98b7f2da1d1888b8"
 
-    if request.method == 'POST':  # only true if form is submitted
-        form = CityForm(request.POST)  # add actual request data to form for processing
-        form.save()  # will validate and save if validate
+    if request.method == 'POST':
+        form = CityForm(request.POST)  # Bind the form with POST data
+        if form.is_valid():  # Validate form data
+            form.save()  # Save valid data to the database
+            return redirect('index')  # Redirect to avoid resubmission on refresh
+        else:
+            form = CityForm()
 
-    form = CityForm()
+
 
     weather_data = []
 
     for city in cities:
-        city_weather = requests.get(
-            url.format(city)).json()  # request the API data and convert the JSON to Python data types
+        try:
+            # Fetch weather data for the city
+            response = requests.get(url.format(city.name))
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+            city_weather = response.json()
 
-        weather = {
-            'city': city,
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
-        }
+            weather = {
+                'city': city.name,
+                'temperature': city_weather['main']['temp'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon']
+            }
 
-        weather_data.append(weather)  # add the data for the current city into our list
+            weather_data.append(weather)
+        except (requests.exceptions.RequestException, KeyError):
+            # Handle API request errors or missing keys gracefully
+            continue
 
     context = {'weather_data': weather_data, 'form': form}
 
-    return render(request, 'weather/index.html', context)  # returns the index.html template
+    return render(request, 'weather/index.html', context)
